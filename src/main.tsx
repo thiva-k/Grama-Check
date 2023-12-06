0004eedbccdfba77996cfe2f427d16ba128a2cad
Chanuka Lakshan Chandra Yapa <chanukachandrayapa@gmail.com>
2023-12-06 16:38:31 +0530

    changed the redirect URLs

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider
       config={{
-        signInRedirectURL: "http://localhost:3000",
-        signOutRedirectURL: "http://localhost:3000",
+        // signInRedirectURL: "http://localhost:3000",
+        // signOutRedirectURL: "http://localhost:3000",
+        signInRedirectURL:
+          "https://ec540c3d-52b5-4f95-8be2-e30f22cbdbc7.e1-us-east-azure.choreoapps.dev",
+        signOutRedirectURL:
+          "https://ec540c3d-52b5-4f95-8be2-e30f22cbdbc7.e1-us-east-azure.choreoapps.dev",
         clientID: "6ZX7RT5D7yFhefksvrk4g9O0EEAa",
         baseUrl: "https://api.asgardeo.io/t/thivaorg",
         scope: ["openid", "profile"],
