87b63ab2b0e9a2c930c66c8503f3d634ab6ad1be
Chanuka Lakshan Chandra Yapa <chanukachandrayapa@gmail.com>
2023-12-06 22:10:53 +0530

    Add privateRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,20 @@ import Home from "./pages/Home";
 import Apply from "./pages/Apply";
 import Status from "./pages/Status";
 import Help from "./pages/Help";
+import { useAuthContext } from "@asgardeo/auth-react";
+
+interface WrapperProps {
+  component: React.ComponentType<any>;
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ component: Component }) => {
+  const { state } = useAuthContext();
+  if (state.isAuthenticated) {
+    return <Component />;
+  } else {
+    return <Home />;
+  }
+};
 
 const App: React.FC = () => {
   return (
@@ -15,8 +29,8 @@ const App: React.FC = () => {
         <Route path="/signin" Component={SignIn} />
         <Route path="/signup" Component={SignUp} />
         <Route path="/" Component={Home} />
-        <Route path="/apply" Component={Apply} />
-        <Route path="/status" Component={Status} />
+        <Route path="/apply" element={<Wrapper component={Apply} />} />
+        <Route path="/status" element={<Wrapper component={Status} />} />
         <Route path="/help" Component={Help} />
       </Routes>
     </Router>
