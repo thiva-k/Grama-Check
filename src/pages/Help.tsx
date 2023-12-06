0e3348b82c4d76eaf7a67b559a496e8688170608
Chanuka Lakshan Chandra Yapa <chanukachandrayapa@gmail.com>
2023-12-07 01:20:38 +0530

    created a header layout

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import BodyLayout from "../components/bodyLayout";
 
 const Help: React.FC = () => {
   return (
     <>
-      <Navbar />
-      <div className="text-center">Slack Messaging</div>
+      <BodyLayout>
+        <Navbar />
+        <div className="text-center">Slack Messaging</div>
+      </BodyLayout>
     </>
   );
 };
