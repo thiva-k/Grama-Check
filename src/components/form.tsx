928b2ed1dd5ae679623515f0964cc33a6f8ec409
Chanuka Lakshan Chandra Yapa <chanukachandrayapa@gmail.com>
2023-12-07 11:04:21 +0530

    minor change

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ApiResponse, submitFormData } from "../api/IdCheckApi";
+// import { ApiResponse, submitFormData } from "../api/IdCheckApi";
 import { useAuthContext } from "@asgardeo/auth-react";
 
 const Form: React.FC = () => {
@@ -9,38 +9,38 @@ const Form: React.FC = () => {
   const { getAccessToken } = useAuthContext();
 
   const handleSubmit = async () => {
-     getAccessToken()
-       .then((token) => {
-         // Token is the resolved value of the promise
-         console.log("Access Token:", token);
-         const apiUrl = `https://7902e7c7-f73b-401f-a1db-07c524deb30a-prod.e1-us-east-azure.choreoapis.dev/rkjj/id-check/endpoint-9090-803/v1/checkNic/${id}`;
+    getAccessToken()
+      .then((token) => {
+        // Token is the resolved value of the promise
+        console.log("Access Token:", token);
+        const apiUrl = `https://7902e7c7-f73b-401f-a1db-07c524deb30a-prod.e1-us-east-azure.choreoapis.dev/rkjj/id-check/endpoint-9090-803/v1/checkNic/${id}`;
 
-         // Make the API request with the obtained access token
-         return fetch(apiUrl, {
-           method: "GET",
-           headers: {
-             Authorization: `Bearer ${token}`,
-             accept: "application/json",
-           },
-         });
-       })
-       .then((response) => {
-         // Check if the API request was successful
-         if (!response.ok) {
-           throw new Error(`HTTP error! Status: ${response.status}`);
-         }
+        // Make the API request with the obtained access token
+        return fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            accept: "application/json",
+          },
+        });
+      })
+      .then((response) => {
+        // Check if the API request was successful
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-         // Parse the JSON response
-         return response.json();
-       })
-       .then((data) => {
-         // Handle the data from the API response
-         console.log("API Response:", data);
-       })
-       .catch((error) => {
-         // Handle errors
-         console.error("Error:", error.message);
-       });
+        // Parse the JSON response
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the data from the API response
+        console.log("API Response:", data);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error("Error:", error.message);
+      });
   };
   return (
     <form
