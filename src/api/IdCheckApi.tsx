4ce0fbad4bb60c3b5f2b42d39e416a04051c18e2
Chanuka Lakshan Chandra Yapa <chanukachandrayapa@gmail.com>
2023-12-07 16:46:32 +0530

    changed API call format

--- src/api/IdCheckApi.tsx
+++ src/api/IdCheckApi.tsx
@@ -1,37 +1,38 @@
 import { useAuthContext } from "@asgardeo/auth-react";
 
-export const submitFormData = async (id: string) => {
-  const { getAccessToken } = useAuthContext();
-  getAccessToken()
-    .then((token) => {
-      // Token is the resolved value of the promise
-      const apiUrl = `https://7902e7c7-f73b-401f-a1db-07c524deb30a-prod.e1-us-east-azure.choreoapis.dev/rkjj/id-check/endpoint-9090-803/v1/checkNic/${id}`;
+export const handleIdCheck = async (
+  nic: string,
+  name: string,
+  address: string
+) => {
+  try {
+    const { getAccessToken } = useAuthContext();
+    const token = await getAccessToken();
 
-      // Make the API request with the obtained access token
-      return fetch(apiUrl, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          accept: "application/json",
-        },
-      });
-    })
-    .then((response) => {
-      // Check if the API request was successful
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+    const apiUrl =
+      "https://7902e7c7-f73b-401f-a1db-07c524deb30a-dev.e1-us-east-azure.choreoapis.dev/rkjj/id-check/endpoint-9090-803/v1/nicCheck";
 
-      // Parse the JSON response
-      return response.json();
-    })
-    .then((data) => {
-      // Handle the data from the API response
-      console.log("API Response:", data);
-      alert("IDCheckAPI Response: " + JSON.stringify(data));
-    })
-    .catch((error) => {
-      // Handle errors
-      console.error("Error:", error.message);
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        accept: "application/json",
+      },
+      body: JSON.stringify({ nic, name, address }),
     });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("API Response Post:", data);
+
+    // Print the response in the console
+    return data;
+  } catch (error) {
+    console.error("Error:", (error as Error).message);
+    throw error;
+  }
 };
