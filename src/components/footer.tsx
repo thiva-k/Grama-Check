5f6b3ddcf5d8e68ed62b4ae14a48d03a409d17e6
Chanuka Lakshan Chandra Yapa <chanukachandrayapa@gmail.com>
2023-12-07 15:33:10 +0530

    moved the api call to API folder-sus

---
+++ src/components/footer.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-white  dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023{" "}
+          <a href="#" className="hover:underline">
+            Grama-Check
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Team
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -3,22 +3,27 @@ import React from "react";
 import Navbar from "../components/navbar";
 import Form from "../components/form";
 import BodyLayout from "../components/bodyLayout";
+import FadeInTransition from "../components/fadeInTrans";
 
 const Apply: React.FC = () => {
   return (
     <>
       <BodyLayout>
         <Navbar />
-        <div className=" py-8 px-16 w-full content-center items-start text-center md:text-left">
-          <h1 className="my-4 text-xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl font-bold leading-tight text-center">
-            To Request a Grama Sevaka Certificate, Fill the below Application
-            with Correct Details and Submit.
-          </h1>
-        </div>
+        <FadeInTransition>
+          <div className=" py-8 px-16 w-full content-center items-start text-center md:text-left">
+            <h1 className="my-4 text-xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl font-bold leading-tight text-center">
+              To Request a Grama Sevaka Certificate, Fill the below Application
+              with Correct Details and Submit.
+            </h1>
+          </div>
+        </FadeInTransition>
       </BodyLayout>
-      <div className="p-4">
-        <Form />
-      </div>
+      <FadeInTransition>
+        <div className="p-4">
+          <Form />
+        </div>
+      </FadeInTransition>
     </>
   );
 };
