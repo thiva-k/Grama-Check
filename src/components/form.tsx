ec3dfc63a021e9f3b253f854d4afd44676794d3e
thiva-k <kalyanasundaramthivaharan@gmail.com>
2023-12-07 14:21:21 +0530

    post method for nic

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -9,39 +9,73 @@ const Form: React.FC = () => {
   const { getAccessToken } = useAuthContext();
 
   const handleSubmit = async () => {
-    getAccessToken()
-      .then((token) => {
-        // Token is the resolved value of the promise
-        const apiUrl = `https://7902e7c7-f73b-401f-a1db-07c524deb30a-prod.e1-us-east-azure.choreoapis.dev/rkjj/id-check/endpoint-9090-803/v1/checkNic/${id}`;
+    try {
+      // Obtain the access token
+      const token = await getAccessToken();
 
-        // Make the API request with the obtained access token
-        return fetch(apiUrl, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            accept: "application/json",
-          },
-        });
-      })
-      .then((response) => {
-        // Check if the API request was successful
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+      // API endpoint for the POST request
+      const apiUrl = "https://7902e7c7-f73b-401f-a1db-07c524deb30a-prod.e1-us-east-azure.choreoapis.dev/rkjj/id-check/endpoint-9090-803/v1/checkNic";
 
-        // Parse the JSON response
-        return response.json();
-      })
-      .then((data) => {
-        // Handle the data from the API response
-        console.log("API Response:", data);
-        alert("IDCheckAPI Response: " + JSON.stringify(data));
-      })
-      .catch((error) => {
-        // Handle errors
-        console.error("Error:", error.message);
+      // Make the API request with the obtained access token
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          accept: "application/json",
+        },
+        body: JSON.stringify({ id, name, address }),
       });
+
+      // Check if the API request was successful
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // Parse the JSON response
+      const data = await response.json();
+      console.log("API Response:", data);
+      alert("IDCheckAPI Response: " + JSON.stringify(data));
+    } catch (error:any) {
+      // Handle errors
+      console.error("Error:", error.message);
+    }
   };
+
+  // const handleSubmit = async () => {
+  //   getAccessToken()
+  //     .then((token) => {
+  //       // Token is the resolved value of the promise
+  //       const apiUrl = `https://7902e7c7-f73b-401f-a1db-07c524deb30a-prod.e1-us-east-azure.choreoapis.dev/rkjj/id-check/endpoint-9090-803/v1/checkNic/${id}`;
+
+  //       // Make the API request with the obtained access token
+  //       return fetch(apiUrl, {
+  //         method: "GET",
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //           accept: "application/json",
+  //         },
+  //       });
+  //     })
+  //     .then((response) => {
+  //       // Check if the API request was successful
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+
+  //       // Parse the JSON response
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       // Handle the data from the API response
+  //       console.log("API Response:", data);
+  //       alert("IDCheckAPI Response: " + JSON.stringify(data));
+  //     })
+  //     .catch((error) => {
+  //       // Handle errors
+  //       console.error("Error:", error.message);
+  //     });
+  // };
   return (
     <form
       className="max-w-md mx-auto shadow-lg hover:shadow-xl transform transition duration-300 ease-in-out p-4 rounded-3xl"
