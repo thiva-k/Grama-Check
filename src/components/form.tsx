8c6097cbd322bef99e1e57b78033ae6b56d54d64
thiva-k <kalyanasundaramthivaharan@gmail.com>
2023-12-08 13:37:41 +0530

    Updated identity api and web app for intern org

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -42,7 +42,7 @@ const Form: React.FC = () => {
       setPoliceCheckStatus(policeCheckData.status === "Accept" ? "You have been validated" : `Police Check Status: ${policeCheckData.status}`);
 
       // ID Check API endpoint
-      const idCheckApiUrl = "https://7902e7c7-f73b-401f-a1db-07c524deb30a-dev.e1-us-east-azure.choreoapis.dev/rkjj/id-check/endpoint-25416-e8a/v1.1/nicCheck";
+      const idCheckApiUrl = "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/gich/gramacheckidentitycheck/endpoint-25416-e8a/v1.0/nicCheck";
 
       // ID Check API request
       const idCheckApiResponse = await fetch(idCheckApiUrl, {
