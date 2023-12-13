53396db6bca1b7e867169b3fc6d626ab71c74669
Chanuka Lakshan Chandra Yapa <chanukachandrayapa@gmail.com>
2023-12-14 01:20:17 +0530

    getStaus API test 1

---
+++ src/api/getStatus.tsx
@@ -0,0 +1,32 @@
+export const performGetStatus = async (
+  token: string,
+  nic: string,
+) => {
+  const getStatusApiUrl =
+    "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/gich/address-check/endpoint-3000-197/v1.0/getStatus";
+
+  try {
+    const getStatusResponse = await fetch(getStatusApiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        accept: "application/json",
+      },
+      body: JSON.stringify({
+        nic,
+      }),
+    });
+
+    if (!getStatusResponse.ok) {
+      throw new Error(`HTTP error! Status: ${getStatusResponse.status}`);
+    }
+
+    const getStatusResult = await getStatusResponse.json();
+    console.log("getStatus", getStatusResult);
+    return getStatusResult;
+  } catch (error) {
+    console.error("Error in get status:", error);
+    throw error;
+  }
+};
--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,14 +1,181 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/navbar";
 import BodyLayout from "../components/bodyLayout";
 import StatusBox from "../components/statusbox";
 import FadeInTransition from "../components/fadeInTrans";
 import Footer from "../components/footer";
 import { useStatusItems } from "../utils/statusContext";
+import { performGetStatus } from "../api/getStatus";
+
+
+interface ApiResponseItem {
+  id: number;
+  user_id: string;
+  police_check_status: number;
+  id_check_status: number;
+  address_check_status: number;
+}
+
+interface ApiResult {
+  result: ApiResponseItem[];
+}
+interface StatusItem {
+  certificateNumber: string;
+  idCheckStatus: string;
+  addressCheckStatus: string;
+  policeCheckStatus: string;
+}
+
+// const apiresp = {
+//   result: [
+//     {
+//       id: 1,
+//       user_id: "123456789V",
+//       police_check_status: 1,
+//       id_check_status: 2,
+//       address_check_status: 2,
+//     },
+//     {
+//       id: 2,
+//       user_id: "123456789V",
+//       police_check_status: 1,
+//       id_check_status: 2,
+//       address_check_status: 1,
+//     },
+//     {
+//       id: 3,
+//       user_id: "123456789V",
+//       police_check_status: 1,
+//       id_check_status: 2,
+//       address_check_status: 1,
+//     },
+//     {
+//       id: 4,
+//       user_id: "123456789V",
+//       police_check_status: 1,
+//       id_check_status: 2,
+//       address_check_status: 1,
+//     },
+//     {
+//       id: 5,
+//       user_id: "123456789V",
+//       police_check_status: 1,
+//       id_check_status: 2,
+//       address_check_status: 1,
+//     },
+//     {
+//       id: 6,
+//       user_id: "123456789V",
+//       police_check_status: 0,
+//       id_check_status: 3,
+//       address_check_status: 0,
+//     },
+//     {
+//       id: 7,
+//       user_id: "123456789V",
+//       police_check_status: 0,
+//       id_check_status: 2,
+//       address_check_status: 0,
+//     },
+//     {
+//       id: 8,
+//       user_id: "123456789V",
+//       police_check_status: 0,
+//       id_check_status: 2,
+//       address_check_status: 0,
+//     },
+//     {
+//       id: 9,
+//       user_id: "123456789V",
+//       police_check_status: 1,
+//       id_check_status: 2,
+//       address_check_status: 0,
+//     },
+//     {
+//       id: 10,
+//       user_id: "123456789V",
+//       police_check_status: 1,
+//       id_check_status: 2,
+//       address_check_status: 0,
+//     },
+//     {
+//       id: 11,
+//       user_id: "123456789V",
+//       police_check_status: 0,
+//       id_check_status: 0,
+//       address_check_status: 0,
+//     },
+//     {
+//       id: 13,
+//       user_id: "123456789V",
+//       police_check_status: 2,
+//       id_check_status: 2,
+//       address_check_status: 1,
+//     },
+//     {
+//       id: 14,
+//       user_id: "123456789V",
+//       police_check_status: 2,
+//       id_check_status: 2,
+//       address_check_status: 1,
+//     },
+//   ],
+// };
 
 const Status: React.FC = () => {
-  const { statusItems } = useStatusItems();
+  const { statusItems, updateStatusItems, token, decodedToken } = useStatusItems();
   console.log(statusItems)
+  
+  const getStatus = async () => {
+    (async (): Promise<void> => {
+      let getStatusResponse;
+      try {
+        if (token !== null) {
+          getStatusResponse = await performGetStatus(token, decodedToken?.nic);
+          console.log("get status response: ", getStatusResponse);
+          const statusItems: StatusItem[] = mapApiToStatusItems(
+            getStatusResponse
+            // apiresp
+          );
+          console.log(statusItems);
+          updateStatusItems(statusItems)
+        } else {
+          console.error("Token is null");
+        }
+      } catch (error) {
+        console.error("Error in component:", error);
+      }
+
+    })();
+  };
+  useEffect(() => {
+    getStatus()
+  }, []);
+
+  const mapApiToStatusItems = (apiResponse: ApiResult): StatusItem[] => {
+    return apiResponse.result.map((apiItem) => ({
+      certificateNumber: `Certificate #${apiItem.id}`,
+      idCheckStatus: mapStatus(apiItem.id_check_status),
+      addressCheckStatus: mapStatus(apiItem.address_check_status),
+      policeCheckStatus: mapStatus(apiItem.police_check_status),
+    }));
+  };
+
+  const mapStatus = (statusCode: number): string => {
+    switch (statusCode) {
+      case 0:
+        return "Declined";
+      case 1:
+        return "Pending";
+      case 2:
+        return "Validated";
+      case 3:
+        return "Paused";
+      default:
+        return "Unknown";
+    }
+  };
+
   return (
     <>
       <BodyLayout>
