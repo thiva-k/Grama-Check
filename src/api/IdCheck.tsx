eb286261e7d509772331c47b74fd8629c42582c7
thiva-k <kalyanasundaramthivaharan@gmail.com>
2023-12-18 10:01:21 +0530

    Update id check api

--- src/api/IdCheck.tsx
+++ src/api/IdCheck.tsx
@@ -3,8 +3,8 @@ export const performIdCheck = async (
   nic: string,
 ) => {
   const idCheckApiUrl =
-    "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/gich/gramacheckidentitycheck/endpoint-10636-12e/v1.1/nicCheck";
-
+    "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/gich/gramacheckidentitycheck/endpoint-7070-070/v1.1/nicCheck";
+  //https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/gich/gramacheckidentitycheck/endpoint-7070-070/v1.1
   try {
     const idCheckResponse = await fetch(idCheckApiUrl, {
       method: "POST",
